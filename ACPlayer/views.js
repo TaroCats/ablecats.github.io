02e680173aa27780f344cc85ca4ea83af498e6cb
AbleCats <etsy@live.com>
2018-06-17 00:53:55 +0800

    Update views.js

--- ACPlayer/views.js
+++ ACPlayer/views.js
@@ -1,12 +1,10 @@
-var bgImageView = {
-    type: "image",
-    props: {
-        id: "bgimg",
-        bgcolor: $color("clear")
-    },
-    layout: $layout.fill,
-}
-
 function bgImageView (){
-    return bgImageView;
+    return bgImageView = {
+        type: "image",
+        props: {
+            id: "bgimg",
+            bgcolor: $color("clear")
+        },
+        layout: $layout.fill,
+    };
 }
